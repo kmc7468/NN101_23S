b582024e3e97a5611f5cc86a52c57db8e5a6daf7
static <kmc7468@naver.com>
2023-03-29 23:51:30 +0900

    3주차 과제 업로드

--- lab/week3/problem1.py
+++ lab/week3/problem1.py
@@ -17,20 +17,43 @@ from typing import Callable
 #        2. In this problem, we will only grade 'predict' function.
 #           Function 'training' is only for modularization.
 
-def training(x_train : list[list[float]], y_train : list[float]) -> tuple[list[float], float]: # DO NOT MODIFY FUNCTION NAME
-    pass ### IMPLEMENT FROM HERE
+def training(x_train, y_train): # DO NOT MODIFY FUNCTION NAME
+    x_train_tensor = torch.tensor(x_train, requires_grad=True)
+    y_train_tensor = torch.tensor(y_train, requires_grad=True)
+    
+    w = torch.tensor([ random() for i in range(len(x_train[0])) ], requires_grad=True)
+    b = torch.tensor(random(), requires_grad=True)
+
+    my_lr = 0.01
+    epoch = 10000
+
+    optimizer = torch.optim.Adam([w, b], lr=my_lr)
+    
+    for i in range(epoch):
+        optimizer.zero_grad()
+
+        y_tensor = 1 / (1 + torch.exp(-(torch.matmul(w, x_train_tensor.transpose(0, 1)) + b)))
+        error = (y_tensor ** (1 - y_train_tensor) + (1 - y_tensor) ** y_train_tensor).sum()
+        error.backward()
+        optimizer.step()
+
+        #if i % 500 == 0:
+        #    print(error)
+    
+    return w.tolist(), b.data.item()
 
 
-def predict(x_train : list[list[float]], y_train : list[float], x_test : list[list[float]]) -> list[float]: # DO NOT MODIFY FUNCTION NAME
+def predict(x_train, y_train, x_test): # DO NOT MODIFY FUNCTION NAME
     w, b = training(x_train, y_train)
-    ### IMPLEMENT FROM HERE
+    
+    return 1 / (1 + torch.exp(-(torch.matmul(torch.tensor(w), torch.tensor(x_test).transpose(0, 1)) + torch.tensor(b))))
 
 if __name__ == "__main__":
     # This is very simple case. Passing this testcase do not mean that the code is perfect.
     # Please consider for the practial problems when score is not high.
     x_train = [[0., 1.], [1., 0.], [2., 5.], [3., 1.], [4., 2.]]
     y_train = [0., 0., 1., 0., 1.]
-    x_test = [[7., 2.], [1.5, 1.], [2.5, 0.5]]
+    x_test = [[0., 1.], [1., 0.], [2., 5.], [3., 1.], [4., 2.], [7., 2.], [1.5, 1.], [2.5, 0.5]]
     
     y_test = predict(x_train, y_train, x_test)
 
